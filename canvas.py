5957916acc7c9c424eb281449578e4d18088bfeb
radiating-pumpkin0Zed <a2zthisisforearningmoney@gmail.com>
2026-03-13 14:15:02 +0000

    added characters other than h

--- canvas.py
+++ canvas.py
@@ -20,15 +20,60 @@ grid = [[(0,0,0) for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
 grid[10][10] = (255,0,0)
 grid[20][20] = (0,255,0)
 grid[30][30] = (0,0,255)
+grid[40][5] = (255,255,0)
 
 clock = pygame.time.Clock()
 
+cursor_x = 5
+cursor_y = 5
+
 while True:
+    
+    if pygame.mouse.get_pressed()[0]:
+
+        mx, my = pygame.mouse.get_pos()
+
+        grid_x = mx // PIXEL_SIZE
+        grid_y = my // PIXEL_SIZE
+        
+        if 0 <= grid_x < GRID_SIZE and 0 <= grid_y < GRID_SIZE:
+            grid[grid_x][grid_y] = (255,255,255)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+
+            key = event.unicode
+            print(key)
+
+            if key == "h":
+                for i in range(5):
+                    for j in range(5):
+                        grid[cursor_x+i][cursor_y+j] = (255,255,255)
+                cursor_x += 6
+
+            elif key == "e":
+                grid[cursor_x+1][cursor_y+1] = (255,255,255)
+                grid[cursor_x+3][cursor_y+1] = (255,255,255)
+                cursor_x += 6
+
+            elif key == "l":
+                for j in range(5):
+                    grid[cursor_x+2][cursor_y+j] = (255,255,255)
+                cursor_x += 6
+
+            elif key == "o":
+                for i in range(5):
+                    grid[cursor_x+i][cursor_y] = (255,255,255)
+                    grid[cursor_x+i][cursor_y+4] = (255,255,255)
+                
+                for j in range(5):
+                    grid[cursor_x][cursor_y+j] = (255,255,255)
+                    grid[cursor_x+4][cursor_y+j] = (255,255,255)
+                cursor_x += 6
 
     screen.fill((30,30,30))
 
@@ -43,5 +88,13 @@ while True:
                     (x*PIXEL_SIZE, y*PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE)
             )
 
+            pygame.draw.rect(
+                    screen,
+                    (50,50,50),
+                    (x*PIXEL_SIZE, y*PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE),
+                    1
+            )
+
+    
     pygame.display.update()
     clock.tick(60)
